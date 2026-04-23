c25faabd3b886f96506e2e7718982b379fe08124
bigdog2809 <shmulever.eitan@gmail.com>
2026-04-23 15:48:00 +0300

    Update voice.py

--- voice.py
+++ voice.py
@@ -5,21 +5,9 @@ engine = pyttsx3.init()
 
 # VOICE
 voices = engine.getProperty('voices') 
-engine.setProperty('voice', voices[3].id)   # changing index, changes voices. 1 for female
+engine.setProperty('voice', voices[2].id)   # changing index, changes voices. 1 for female
 
 def say(text, sleep_time=2):
-    engine.say(text)
-    time.sleep(sleep_time)
-    engine.runAndWait()
-
-
-
-voices = engine.getProperty('voices') 
-engine.setProperty('voice', voices[0].id)  
-
-
-
-def say_en(text, sleep_time=2):
     engine.say(text)
     time.sleep(sleep_time)
     engine.runAndWait()
